1574ea807a601559e15ecb0270efdb20e910bd7e
Robert Wongus <rob_wo@yahoo.com>
2020-04-03 16:59:25 -0400

    added member html files

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const render = require("./lib/htmlRenderer");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -15,6 +16,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 // 3. all of your js files except for app.js, should be in the lib directory.
 // 4. this means that you'll have to fix the imports above, too.
 // So: const Manager = require("./lib/Manager"), for example.
+//DONE!
 
 //const render = require("./lib/htmlRenderer");
 
